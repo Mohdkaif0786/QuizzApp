0031c45ac3619e39b1a544a7ffdc0fb614e223f9
Mohdkaif0786 <kmd060463@gmail.com>
2024-02-17 07:47:52 +0000

    upload quizz app

---
+++ question.js
@@ -0,0 +1,54 @@
+// QUESTION START
+let questions = [
+  {
+    num: 1,
+    question: "What does the abbreviation HTML stand for?",
+    answer: "HyperText Markup Language.",
+    Options: [
+      "High Text Markup Language.",
+      "HyperText Markup Language.",
+      "Hiper Text Markdown Language.",
+      "None of the Above",
+    ],
+  },
+  {
+    num: 2,
+    question: "How many sizes of headers are available in HTML by default?",
+    answer: "6",
+    Options: ["5", "4", "6", "3"],
+  },
+  {
+    num: 3,
+    question: "Javascript is an _______ language?",
+    answer: "Object-Oriented",
+    Options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the Above",
+    ],
+  },
+  {
+    num: 4,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "const",
+    Options: ["var", "const", "let", "constant"],
+  },
+  {
+    num: 5,
+    question: "Who is the first education minister of india?",
+    answer: "Maulana Abul Kalam Azad",
+    Options: [
+      "Rajendra Prasad",
+      "Maulana Abul Kalam Azad",
+      "Ram Prasad Bismil",
+      "Sradar Vallabhbhai Patel",
+    ],
+  },
+  {
+    num: 6,
+    question: "Who is the current president of india?",
+    answer: "Droupadi Murmu",
+    Options: ["Ram Nath Kovind", "V.V Giri", "Droupadi Murmu", "Rajnath singh"],
+  },
+];
